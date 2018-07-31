667b2d85bb70cdad828f7b38961a5fd2367a474f
James <jimblackwood3@gmail.com>
2018-07-31 05:29:33 -0700

    added basic db conn

--- app/Main.hs
+++ app/Main.hs
@@ -10,6 +10,7 @@
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
 
+
 import           Web.Spock
 import           Web.Spock.Config
 
@@ -25,26 +26,51 @@ import           Database.Persist.TH
 
 
 
-data Fund = Fund {
-	name :: String,
-	age  :: Int
-} deriving (Generic, Show)
-instance ToJSON Fund
-instance FromJSON Fund
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Fund json
+    name String
+    age  Int
+    deriving Show
+|]
 
-type Api = SpockM () () () ()
+type Api = SpockM SqlBackend () () ()
 
-type ApiAction a = SpockAction () () () a
+type ApiAction a = SpockAction SqlBackend () () a
+
+runSQL :: (HasSpock m, SpockConn m ~ SqlBackend) => SqlPersistT (LoggingT IO) a -> m a
+runSQL action = runQuery $ \conn -> runStdoutLoggingT $ runSqlConn action conn
+
+
+errorJson :: Int -> Text -> ApiAction ()
+errorJson code message =
+    json $
+      object
+      ["result" .= String "failure"
+      , "error" .= object ["code" .= code, "message" .= message]
+      ]
+
 
 main :: IO ()
 main = do
-  spockCfg <- defaultSpockCfg () PCNoDatabase ()
+  pool <- runStdoutLoggingT $ createSqlitePool "api.db" 5
+  spockCfg <- defaultSpockCfg () (PCPool pool) ()
+  runStdoutLoggingT $ runSqlPool (do runMigration migrateAll) pool
   runSpock 8080 (spock spockCfg app)
 
 app :: Api
 app = do
   get "funds" $ do
-    json $ Fund { name = "Credit Opps", age = 25 }
+    allFunds <- runSQL $ selectList [] [Asc FundId]
+    json allFunds
   post "funds" $ do
-    theFund <- jsonBody' :: ApiAction Fund
-    text $ "Parsed: " <> pack (show theFund)
+    maybeFund <- jsonBody :: ApiAction (Maybe Fund)
+    case maybeFund of
+        Nothing -> errorJson 1 "Failed to parse request body as fund"
+        Just theFund -> do
+            newId <- runSQL $ insert theFund
+            json $ object ["result" .= String "success", "id" .= newId]
+  get ("funds" <//> var) $ \fundId -> do
+    maybeFund <- runSQL $ P.get fundId :: ApiAction (Maybe Fund)
+    case maybeFund of
+        Nothing -> errorJson 2 "Could not find fund with id"
+        Just theFund -> json theFund
